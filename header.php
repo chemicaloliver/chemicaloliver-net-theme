c5628a837520a5dfd9cce6466d79537afc7341c1
Oliver Smith <oliversmith@olive-thinkpad>
2012-05-06 15:52:00 +0100

    Added github fork me ribbon

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 <!--[if IE 8 ]>    <body <?php body_class('ie8'); ?>> <![endif]-->
 <!--[if IE 9 ]>    <body <?php body_class('ie9'); ?>> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <body <?php body_class('ie6'); ?>> <!--<![endif]-->
-
+<a href="https://github.com/chemicaloliver"><img style="position: fixed; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/camo.github.com/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub"></a>
 <header role="banner"> <div class="container">
         
       <h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
